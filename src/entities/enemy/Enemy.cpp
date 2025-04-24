19532ef3e2710c98837d492f43cfef01fe940a78
farlo0nn <farlo0nn@gmail.com>
2025-04-24 14:53:18 +0200

    implementing enemy #1

--- src/entities/enemy/Enemy.cpp
+++ src/entities/enemy/Enemy.cpp
@@ -1,3 +1,34 @@
-#include "./Enemy.h"
+#include "Enemy.h"
+#include "../../Constants.cpp"
 
-Enemy::Enemy(EnemyPosition position, Word word) : position(position), lives(1), word(word) {}
+Enemy::Enemy(EnemyPosition position, const Word& word, const sf::Texture& texture)
+    : m_position(position), m_word(word), m_lives(1)
+{
+    setTexture(texture);
+    setPosition(position.coordinates);  // Helper function
+}
+
+void Enemy::update(float deltaTime, int round) {
+
+    move(m_position.direction * BASE_SPEED * round * 0.4f * deltaTime);
+}
+
+int Enemy::get_lives() const {
+    return m_lives;
+}
+
+void Enemy::damage(int amount) {
+    m_lives -= amount;
+}
+
+bool Enemy::is_dead() const {
+    return m_lives <= 0;
+}
+
+EnemyPosition Enemy::get_enemy_position() const {
+    return m_position;
+}
+
+const Word& Enemy::get_word() const {
+    return m_word;
+}
--- src/entities/enemy/Enemy.h
+++ src/entities/enemy/Enemy.h
@@ -1,12 +1,23 @@
 #pragma once
 #include "EnemyPosition.h"
 #include "../../logic/Word.h"
+#include <SFML/Graphics.hpp>
 
-
-class Enemy {
+class Enemy : public sf::Sprite {
 public:
-    EnemyPosition position;
-    Word word;
-    int lives;
-    Enemy(EnemyPosition position, Word word);
+
+    Enemy(EnemyPosition position, const Word& word, const sf::Texture& texture);
+
+    auto update(float deltaTime, int round) -> void;
+    auto get_lives() const -> int;
+    auto damage(int amount) -> void;
+    auto is_dead() const -> bool;
+
+    auto get_enemy_position() const -> EnemyPosition;
+    auto get_word() const -> const Word&;
+
+private:
+    EnemyPosition m_position;
+    Word m_word;
+    int m_lives;
 };
